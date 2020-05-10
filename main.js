5788613158c8b29d5e8af7b353ee5a9f7e4a8097
Junya Oiwa <0920oiwa@gmail.com>
2020-05-10 18:02:18 +0900

    [add]必要なライブラリをインストール

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 require('dotenv').config();
 
+const fs = require('fs');
+const axios = require('axios');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const diff = require('diff');
+
+async function main() {
+  
+}
+
+main();
